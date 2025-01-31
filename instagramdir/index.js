1c6f81395618607aad15dd1d9e7e50da5df91c0a
Raju kumar <rajusharmadata123@gmail.com>
2025-01-31 06:07:19 +0530

    add

---
+++ instagramdir/index.js
@@ -0,0 +1,28 @@
+const express  =  require("express");
+
+const app = express();
+
+let port = 8080;
+
+app.listen(port,(req,res) => {
+    console.log("app is listing");
+});
+
+app.get("/", (req,res) =>{
+    res.send("This is my home page ");
+});
+
+app.get("/ig/:username",(req,res)=>{
+    let {username} = req.params;
+    // console.log(username);
+    const instadata = require("./data.json");
+   const data = instadata[username];
+ 
+    if(data){
+        res.render("instagram.ejs",{data});
+    }else{
+        res.render("error.ejs");
+    }
+   
+   
+})
