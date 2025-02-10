d21f97e7ef9e6242df79dbae9e3f87006d268943
Raju kumar <rajusharmadata123@gmail.com>
2025-02-10 15:08:41 +0530

    add new files

--- instagramdir/index.js
+++ instagramdir/index.js
@@ -3,6 +3,8 @@ const express  =  require("express");
 const app = express();
 
 let port = 8080;
+app.use(express.static("public"));
+// app.set("views",path.join(__dirname,"/views"));
 
 app.listen(port,(req,res) => {
     console.log("app is listing");
